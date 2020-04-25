08b2ab2cf821214f5a8db15d940aa4c3d56f5ea7
Marcus Lau <lau.marcus76@gmail.com>
2020-04-25 00:10:51 +0000

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -10,4 +10,6 @@ require 'pry'
 
 def get_the_min(groceries)
   #code your solution here!
+  # hash.key.min returns the value of the lowest value key
+  groceries.values.flatten.min
 end
--- lib/values.rb
+++ lib/values.rb
@@ -10,4 +10,6 @@ require 'pry'
 
 def get_the_values(groceries)
   #code your solution here!
+  groceries.values.flatten
+  # hash.key.flatten method takes all elements OUT of nested arrays and returns it into 1 single array
 end
